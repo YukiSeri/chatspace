353342874b6ab79db4ce58687837d56339f228db
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-09-20 17:00:18 +0900

    resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170916171141) do
-=======
-ActiveRecord::Schema.define(version: 20170919070502) do
->>>>>>> master
+Schema.define(version: 20170919070502) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
@@ -38,7 +34,6 @@ ActiveRecord::Schema.define(version: 20170919070502) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
