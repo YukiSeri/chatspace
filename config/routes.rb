811cf795c08b386fa17a6b6e1c9b11ce129916ba
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-09-21 09:40:59 +0900

    create groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'messages#index'
   resources :messages, only: [:index]
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
