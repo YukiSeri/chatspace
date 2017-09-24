f2373a951be0fe0a7001013df74d9a112e1106e5
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-09-24 19:11:51 +0900

    save data about group_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,6 @@ class GroupsController < ApplicationController
   private
 
   def create_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
