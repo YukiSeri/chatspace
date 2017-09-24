57a415a8b8eed979d6749177da6cd18daecff35a
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-09-24 21:01:42 +0900

    add validation and error_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true
+
   has_many :group_users, dependent: :delete_all
   has_many :users, through: :group_users
 end
