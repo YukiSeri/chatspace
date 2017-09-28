67c6fe264663a1428ec1024711829af364b39931
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-09-28 13:30:38 +0900

    create partial-template of side and change messages#index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,11 @@
 class MessagesController < ApplicationController
   def index
     @message = Message.new
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+  end
+
+  def create
+
   end
 end
