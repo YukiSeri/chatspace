32c4cb5a0d6552da34f47357bfdd094d19e132d4
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-09-29 22:18:11 +0900

    create message-text-send-create_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,18 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    binding.pry
+    if @message.save
+      redirect_to group_messages_path(params[:group_id])
+    else
+      flash[:alert]="メッセージを入力してください。"
+      render :index
+    end
+  end
 
+  private
+  def message_params
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
