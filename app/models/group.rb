844cc85cbc24b573a196ade030db2a4ebd4b8547
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-10-03 23:38:45 +0900

    add last_message in side-bar

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,4 +7,15 @@ class Group < ApplicationRecord
   has_many :messages
 
   scope :desc, -> { order("groups.updated_at DESC") }
+
+  def last_message
+    if messages.last.try(:text)
+      messages.last.text
+    elsif messages.last.try(:image)
+      '画像が投稿されています。'
+    else
+      'まだメッセージはありません'
+    end
+  end
+
 end
