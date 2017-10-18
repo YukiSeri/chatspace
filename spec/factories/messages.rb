4f8d8a9894950d9b609ed675aba2cd2b64cbc85e
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-10-18 10:03:23 +0900

    create association in rspec

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
   factory :message do
     text  { Faker::Lorem.sentence }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image/test.jpg')) }
-    association :group, factory: :group, id: 1
-    association :user, factory: :user, id: 1
+    association :group, factory: :group
+    association :user, factory: :user
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,12 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password              { Faker::Internet.password(8) }
     password_confirmation { password }
+
+    # 多対多の関係をuserから一括生成する
+    # @groups = current_user.groupsを実装
+    after(:create) do |user|
+      create(:group_user, user: user, group: create(:group))
+    end
   end
 
 end
