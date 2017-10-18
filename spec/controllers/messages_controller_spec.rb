1a4772c17de9399bb23ecd9014a8e6ee86f88cda
Yuki Seri <yuki.tobe.myself@gmail.com>
2017-10-18 11:22:00 +0900

    renders the :index template, but error

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,17 +2,32 @@ require 'rails_helper'
 
 describe MessagesController do
   let(:user) { create(:user) }
-  # userをcreateし、let内に格納
+  let(:group) { create(:group) }
+  let(:message) { create(:message) }
 
-  context 'When user can log-in' do
-    before do
-      login_user(user)
-      # controller_macros.rb内のlogin_userメソッドを呼び出し
+  describe 'GET #index' do
+    context 'When user can log-in' do
+      before do
+        login_user(user)
+      end
+
+      it "renders the :index template" do
+        get :index, params: { group_id: group }
+        expect(response).to render_template :index
+      end
+
+      # it "assigns the requested contact to @groups" do
+      #   @group = user.groups.first
+      #   get :index, params: { group_id: @group.id }
+      #   groups = user.groups
+      #   except(assigns(:groups)).to eq groups
+      # end
+
     end
-  end
 
-  context 'When user dont log-in' do
+    context 'When user dont log-in' do
 
+    end
   end
 
 end
